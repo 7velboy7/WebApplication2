90dd115ee0fa95066d6b4161b8c3339c935841f7
Alex Velboy <93401120+7velboy7@users.noreply.github.com>
2022-11-05 02:01:07 +0200

    !

--- AuthOptions.cs
+++ AuthOptions.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using System.Text;
+﻿//using Microsoft.IdentityModel.Tokens;
+//using System.Text;
 
-namespace WebApplication1
-{
-    public class AuthOptions // should deleted if not used, but it still needed for some postman tests
-    {
-        public const string ISSUER = "AlexsServer";
-        public const string AUDIENCE = "DSSClients";
-        const string KEY = "secret_key";
-        public static SymmetricSecurityKey GetSymetricSecurityKey() =>
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+//namespace WebApplication1
+//{
+//    public class AuthOptions // should deleted if not used, but it still needed for some postman tests
+//    {
+//        public const string ISSUER = "AlexsServer";
+//        public const string AUDIENCE = "DSSClients";
+//        const string KEY = "secret_key";
+//        public static SymmetricSecurityKey GetSymetricSecurityKey() =>a
+//            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
 
-    }
-}
+//    }
+//}
